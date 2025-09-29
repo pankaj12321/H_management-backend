e1126da3bc5e32d30bc216deff395032b403a83a
pankaj kuamawat <pankaj.k@airsme.in>
2025-09-29 12:32:12 +0530

    done

--- app.js
+++ app.js
@@ -95,9 +95,7 @@ app.use((req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-  // app.get('*', (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
-  // );
+
 }
 
 app.listen(PORT, () => {
